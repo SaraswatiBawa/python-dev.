ad6d614adba8fda26b8e71145f2961ac0626eb56
SaraswatiBawa <117761638+SaraswatiBawa@users.noreply.github.com>
2022-11-09 19:18:51 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+light=str(input("enter the light color:"))
+if(light =="green"):
+    print("car can allowed to go")
+if(light== "red"):
+    print("car stop wait")
+if(light== "yellow"):
+    print("car has to wait")
+
+
